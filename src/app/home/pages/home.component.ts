cb2d03b3196ae1ce2562f9bb400385fa190d4a87
ADMIN-PC\hohuyluat <hohuyluat>
2025-02-11 17:08:35 +0700

    update ngày 11/2/2025

--- src/app/home/pages/home.component.ts
+++ src/app/home/pages/home.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  images = [
+    '/assets/image/0e3d2401733e61c3dc9da66a609f85f0.jpg',
+    '/assets/image/nha-hang-ca-ro-dong-1725421564512.png',
+    '/assets/image/12270f80d3f516857d009eb21dfdc5d9.jpg',
+    '/assets/image/images.jpg'
+  ];
+  currentIndex = 0;
 
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
   constructor() { }
 
   ngOnInit(): void {
