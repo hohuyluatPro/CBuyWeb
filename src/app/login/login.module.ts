d2c69d9d2a157ff9e44ac6eedbbd5efc4c52fdc9
ADMIN-PC\hohuyluat <hohuyluat>
2025-02-18 17:07:26 +0700

    commit ngay 18/2/205 v2

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { ReactiveFormsModule } from '@angular/forms'; // Import vào đây
-
+import { MatIconModule } from '@angular/material/icon'
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class LoginModule { }
